badc4024481d0fa7dc2351bc902380e9a9eebee7
xinghaidu <2314069348@qq.com>
2023-02-23 01:42:23 +0800

    no message

---
+++ src/components/dialog.vue
@@ -0,0 +1,122 @@
+<template>
+  <div v-if="isShowDialog" class="dialog-white">
+    <div class="dialog" :style="setStyle">
+      <div class="header">
+        <div class="title">{{ dialogTitle }}</div>
+        <div class="close" @click="close"></div>
+      </div>
+      <div class="content">
+        <slot name="content"></slot>
+      </div>
+      <div v-if="showBtn" class="footer">
+        <slot name="footer"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Dialog",
+  components: {},
+  props: {
+    isShowDialog: {
+      type: Boolean,
+      default: false
+    },
+    dialogTitle: {
+      type: String,
+      default: ''
+    },
+    dialogWidth: {
+      type: String,
+      default: '30'
+    },
+    dialogHeight: {
+      type: String,
+      default: '50'
+    },
+    showBtn: {
+      type:Boolean,
+      default:true
+    }
+  },
+  data() {
+    return {}
+  },
+  computed: {
+    setStyle() {
+      return {
+        width: this.dialogWidth + 'vw',
+        height: this.dialogHeight + 'vh'
+      }
+    }
+  },
+  methods: {
+    close() {
+      this.$emit('close',false)
+    }
+  }
+}
+</script>
+<style scoped>
+.dialog-white {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  overflow: auto;
+  z-index: 2007;
+  margin: 0;
+  text-align: justify;
+}
+
+.dialog {
+  position: relative;
+  margin: 0 auto;
+  padding: 20px;
+  margin-top: 25vh;
+  background-color: #fff;
+  border-radius: 10px;
+  box-sizing:border-box;
+  box-shadow: 0 1px 3px rgba(0,0,0,.3);
+}
+.header {
+  width: 100%;
+  font-size: 1.2vw;
+  line-height: 1.5vw;
+}
+.title {
+  float: left;
+}
+.close {
+  float: right;
+  width: 1vw;
+  height: 1vw;
+  background: url(../assets/img/close.svg) no-repeat;
+  background-size: cover;
+}
+.close:hover {
+  background: url(../assets/img/close1.svg) no-repeat;
+  background-size: cover;
+}
+.close:active {
+  background: url(../assets/img/close1.svg) no-repeat;
+  background-size: cover;
+}
+.content {
+  width: 100%;
+  font-size: 1.2vw;
+  margin-top: 20px;
+}
+.footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin-bottom: 30px;
+  text-align: center;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,214 @@
+<template>
+  <div class="home">
+    <canvas ref="canvas"></canvas>
+    <div class="container">
+      <div class="content" ref="content">
+        <span>2023年1月25日，浓烈的春节气氛下我们相遇。</span>
+        <span>从我第一次遇见你，我就觉得你是我命中注定的唯一</span>
+        <span>在我们相处的这段日子里，从一开始互相有所顾虑以致我差点错过你，到后面我们第一次一起出去游玩，再到后面与你一起度过第一个情人节，到如今无话不谈。</span>
+        <span>在接下来的日子里，我希望我的身边能够有你的身影，也希望你的身边有我，可以继续相互分享喜怒哀乐。</span>
+        <span>所以我想让你做我的女朋友，成为我的宝，你愿意吗？宝</span>
+      </div>
+      <div v-if="showBtn" class="btns">
+        <div class="btn btn-green" @click="handleClick(0)">答应他</div>
+        <div class="btn btn-blue" @click="handleClick(1)">再等等</div>
+        <div class="btn btn-grey" @click="handleClick(2)">算了</div>
+      </div>
+    </div>
+    <Dialog :is-show-dialog="waitDialog" :show-btn="waitBtn" @close="val=> waitDialog = val">
+      <template v-slot:content>
+        <div>{{ waitContent }}</div>
+      </template>
+      <template v-slot:footer>
+        <div class="btn btn-green" @click="handleClickWait(0)">答应他</div>
+        <div class="btn btn-blue" @click="handleClickWait(1)">再等等</div>
+      </template>
+    </Dialog>
+    <Dialog :is-show-dialog="errorDialog" :show-btn="errorBtn" dialog-title="给个机会吧" @close="val=>errorDialog = val">
+      <template v-slot:content>
+        <div>{{ errorContent }}</div>
+      </template>
+      <template v-slot:footer>
+        <div class="btn btn-green" @click="handleClickError(0)">答应他</div>
+        <div class="btn btn-grey" @click="handleClickError(1)">算了</div>
+      </template>
+    </Dialog>
+  </div>
+</template>
+
+<script>
+import { createHeart } from "../script/heart"
+import Dialog from '../components/dialog.vue'
+export default {
+  name: 'HomeView',
+  components: {
+    Dialog
+  },
+  data() {
+    return {
+      canvasRef: '',
+      ctx: '',
+      ww: '',
+      wh: '',
+      hearts: [],
+      heartImage: new Image(),
+      content: '2023年1月25日，浓烈的春节气氛下我们相遇。从我第一次遇见你，我就觉得你是我命中注定的唯一。在我们相处的这段日子里，从一开始互相有所顾虑以致我差点错过你，到后面我们第一次一起出去游玩，再到后面与你一起度过第一个情人节，到如今无话不谈。在接下来的日子里，我希望我的身边能够有你的身影，也希望你的身边有我，可以继续相互分享喜怒哀乐。所以我想让你做我的女朋友，成为我的宝，你愿意吗？宝',
+      progress: 0,
+      interValId: null,
+      showBtn: false,
+      waitDialog: false,
+      errorDialog: false,
+      waitContent: '还等啥啊，快答应啊！',
+      waitIndex: 0,
+      waitImage: '',
+      waitBtn: true,
+      errorContent: '别啊，我觉得我们可以',
+      errorIndex: 0,
+      errorImage: '',
+      errorBtn: true
+    }
+  },
+  created() {
+  },
+  mounted() {
+    this.canvasRef = this.$refs.canvas
+    this.ctx = this.canvasRef.getContext("2d")
+    this.ww = window.innerWidth
+    this.wh = window.innerHeight
+    window.addEventListener("resize", function () {
+      this.ww = window.innerWidth;
+      this.wh = window.innerHeight;
+    })
+    this.heartImage.onload = this.init();
+    this.heartImage.src = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI0NzMuOHB4IiBoZWlnaHQ9IjQwOC42cHgiIHZpZXdCb3g9IjAgMCA0NzMuOCA0MDguNiI+PHBhdGggZmlsbD0iI2QzMjkzMiIgZD0iTTQwNC42LDE2LjZDMzg1LjQsNi4xLDM2My41LDAsMzQwLDBjLTQxLjUsMC03OC41LDE4LjktMTAzLDQ4LjVDMjEyLjMsMTguOSwxNzUuMywwLDEzMy44LDAgYy0yMy4zLDAtNDUuMyw2LjEtNjQuNSwxNi42QzI3LjksMzkuNSwwLDgzLjQsMCwxMzMuOWMwLDE0LjQsMi40LDI4LjMsNi42LDQxLjJDMjkuNiwyNzguNCwyMzcsNDA4LjYsMjM3LDQwOC42IHMyMDcuMi0xMzAuMiwyMzAuMi0yMzMuNWM0LjMtMTIuOSw2LjYtMjYuOCw2LjYtNDEuMkM0NzMuOCw4My40LDQ0NS45LDM5LjYsNDA0LjYsMTYuNnoiLz48L3N2Zz4=";
+    this.setcontent()
+  },
+  methods: {
+    init() {
+      requestAnimationFrame(this.render);
+      this.canvasRef.width = this.ww;
+      this.canvasRef.height = this.wh;
+      for (var i = 0; i < 100; i++) {
+        this.hearts.push(createHeart(this.ww, this.wh, this.ctx, this.heartImage))
+      }
+    },
+    render() {
+      this.ctx.clearRect(0, 0, this.ww, this.wh);
+      for (var i = 0; i < 100; i++) {
+        this.hearts[i].update();
+        this.hearts[i].draw();
+      }
+      requestAnimationFrame(this.render);
+    },
+    setcontent() {
+      let str = this.$refs.content.innerHTML
+      let progress = 0
+      this.interValId = setInterval(() => {
+        var current = str.substr(progress, 1);
+        if (current == '<') {
+          progress = str.indexOf('>', progress) + 1;
+        } else {
+          progress++;
+        }
+        this.$refs.content.innerHTML = str.substring(0, progress) + (progress & 1 ? '_' : '');
+        if (progress >= str.length) {
+          setTimeout(() => {
+            this.showBtn = true
+          }, 1000);
+          clearInterval(this.interValId);
+        }
+      }, 75);
+    },
+    //点击事件
+    handleClick(val) {
+      this.waitIndex = 0
+      this.errorIndex = 0
+      if (val == 0) {
+        this.$router.push('/success')
+      } else if (val == 1) {
+        this.waitDialog = true
+        this.waitIndex ++
+      } else if (val == 2) {
+        this.errorDialog = true
+        this.errorIndex++
+      }
+    }, 
+    handleClickWait(val) {
+      if (val == 0) {
+        this.waitDialog = false
+        this.$nextTick(() => {
+          this.$router.push('/success')
+        })
+      } else {
+        if(this.waitIndex < 2) {
+        this.waitContent = '别等等了，再等花都谢了'
+        this.waitIndex ++
+      } else {
+        this.waitContent = '我会继续等你，等你的回复'
+        this.waitBtn = false
+        setTimeout(() => {
+          this.waitDialog = false
+        }, 1000*5);
+      }
+      }
+    },
+    handleClickError(val) {
+      debugger
+      if (val == 0) {
+        this.waitDialog = false
+        this.$nextTick(() => {
+          this.$router.push('/success')
+        })
+      } else {
+        if(this.errorIndex == 1) {
+        this.errorContent = '有什么问题请直接对我说，不要这么急着拒绝啊'
+        this.errorIndex ++
+      }else if(this.errorIndex == 2){
+        this.errorContent = '请慎重考虑啊'
+        this.errorIndex++
+      } else {
+        this.errorContent = '对不起，没能和你在一起，祝你能找到更好的另一半，也希望我们还是朋友'
+        this.errorBtn = false
+        setTimeout(() => {
+          this.errorDialog = false
+        }, 1000*5);
+      }
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+.home {
+  background-color: #ffe;
+}
+
+.container {
+  width: 30vw;
+  height: 50vh;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.content {
+  color: #000;
+  font-size: 1.2vw;
+  line-height: 2.4vw;
+  text-align: justify;
+  text-indent: 2.4vw;
+}
+
+.btns {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 6vh;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  font-size: 1.1vw;
+  line-height: 5vh;
+}
+</style>
